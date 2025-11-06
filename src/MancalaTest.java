9d7aa4915d2a31849c58a4135d325b41b7db076f
Rongjie <rongjie.mai@sjsu.edu>
2025-11-05 22:21:20 -0800

    Update MancalaTest.java

--- src/MancalaTest.java
+++ src/MancalaTest.java
@@ -5,8 +5,19 @@
 /**
  *
  */
+import javax.swing.*;
+import java.awt.*;
+
 public class MancalaTest {
     public static void main(String[] args) {
-
+        SwingUtilities.invokeLater(() -> {
+            // Create model, view, controller
+            MancalaModel model = new MancalaModel();
+            MancalaView view = new MancalaView();
+            MancalaController controller = new MancalaController(model, view);
+            
+            // Display style selection first
+            view.showStyleSelection();
+        });
     }
 }
