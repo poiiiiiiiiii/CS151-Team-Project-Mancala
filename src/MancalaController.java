cda3210f4c20934d662fe9ee533337457ec45b37
Rongjie <rongjie.mai@sjsu.edu>
2025-11-05 22:27:14 -0800

    Update MancalaController.java

--- src/MancalaController.java
+++ src/MancalaController.java
@@ -8,4 +8,11 @@
 public class MancalaController {
     private MancalaModel model;
     private MancalaView view;
-}
+    private BoardStyle currentStyle;
+    
+    public MancalaController(MancalaModel model, MancalaView view) {
+        this.model = model;
+        this.view = view;
+        
+        setupControllers();
+    }
