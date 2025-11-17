75ce01698a61f5767dfbdaa1be3dc632fe134a99
sarcrow <contact.sjhoang@gmail.com>
2025-11-16 19:35:20 -0800

    Added controller methods for future implementation

--- src/MancalaController.java
+++ src/MancalaController.java
@@ -9,10 +9,28 @@ public class MancalaController {
     private MancalaModel model;
     private MancalaView view;
     private BoardStyle currentStyle;
-    
+
     public MancalaController(MancalaModel model, MancalaView view) {
         this.model = model;
         this.view = view;
-        
-        setupControllers();
     }
+
+    // handles clicking undo button
+    public void handleUndo() {
+
+    }
+
+    // handles clicking pit and board
+    public void clickBoard() {
+
+    }
+
+    public void newGame() {
+
+    }
+
+    public void chooseStyle() {
+
+    }
+
+}
