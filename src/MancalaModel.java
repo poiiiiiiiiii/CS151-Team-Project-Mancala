692a8db1e368257a97c155d89a9f7b5d6f4e8b79
sarcrow <contact.sjhoang@gmail.com>
2025-11-16 19:32:16 -0800

    Added accessor methods, additional instance variable, adjusted undo variables and implentation

--- src/MancalaModel.java
+++ src/MancalaModel.java
@@ -2,27 +2,41 @@
  *
  */
 
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.util.ArrayList;
+
 /**
  *
  */
 public class MancalaModel {
-    private boolean player1Turn = true;
-    private int undos = 3;
-    private int[] pits = new int[14];
+    private boolean player1Turn;
+    private static final int MAX_UNDOS = 3;
+    private int undoCount;
+    private int[] pits;
     private int[] lastState;
+    private ArrayList<ChangeListener> listeners;
 
     /**
      *
      */
     public MancalaModel () {
+        player1Turn = true;
+        undoCount = 0;
+        pits = new int[14];
+        listeners = new ArrayList<>();
+    }
+
+    public void initialize(int stones) {
         pits[6] = 0; //Player 1 Mancala 0 stones
         pits[13] = 0; // Player 2 Mancala 0 stones
         for (int i = 0; i < 6; i++) {
-            pits[i] = 4; //Player 1 pits 4 stones
+            pits[i] = stones; // add stones to Player 1 pit
         }
         for (int i = 7; i < 13; i++) {
-            pits[i] = 4; // Player 2 pits 4 stones
+            pits[i] = stones; // add stones to Player 2 pit
         }
+        notifyListeners();
     }
 
     /**
@@ -31,9 +45,9 @@ public class MancalaModel {
      */
     public void selectPit(int selected) {
         lastState = pits; //saves the state of game before move is made
-        undos = 3;
+        undoCount = 0;
         int count = pits[selected];
-        pits[selected] = 0;
+        // pits[selected] = 0;
         int index = selected;
 
         for(int i = count; i > 0; i--) {
@@ -48,20 +62,40 @@ public class MancalaModel {
     }
 
     /**
-     *
+     * undoes the players move if the player has undos left on that turn
+     */
+    public void undo() {
+        if (undoCount < MAX_UNDOS) {
+            pits = lastState;
+            undoCount++;
+        }
+    }
+
+    // Accessors
+
+    public int[] getPits() {
+        return pits;
+    }
+
+    public int getUndoCount() {
+        return undoCount;
+    }
+    /**
      * @return true if player 1's turn, false if player 2's turn
      */
     public boolean isPlayer1Turn() {
         return player1Turn;
     }
 
-    /**
-     * undoes the players move if the player has undos left on that turn
-     */
-    public void undo() {
-        if (undos > 0) {
-            pits = lastState;
-            undos--;
+
+    // Observer pattern methods
+    public void attach(ChangeListener listener) {
+        listeners.add(listener);
+    }
+
+    private void notifyListeners(){
+        for (ChangeListener listener : listeners) {
+            listener.stateChanged(new ChangeEvent(this));
         }
     }
 }
